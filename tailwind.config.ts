e60268ba55928ded1cd9f9ae55736d33ee300996
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-07-05 12:32:13 +0300

    add tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,70 @@
+import type { Config } from 'tailwindcss';
+
+const config = {
+  darkMode: ['class'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+
+  ],
+  prefix: '',
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '1280px',
+      xl: '1440px',
+
+      smOnly: { max: '767.98px' },
+      mdOnly: { min: '768px', max: '1279.98px' },
+      notXL: { max: '1279.98px' },
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1.25rem',
+          sm: '1.25rem',
+          md: '2rem',
+          xl: '2rem',
+        },
+      },
+      backgroundImage: {
+        patternLg: `url(/images/pattern.jpg)`,
+        patternSm: `url(/images/pattern1.jpg)`,
+        patternXs: `url(/images/pattern2.jpg)`,
+      },
+      fontFamily: {
+        rajdhani: ['var(--font-rajdhani)'],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      colors: {
+        primary: {
+          DEFAULT: '#222222',
+          hover: '#2f2f2f',
+        },
+        accent: {
+          DEFAULT: '#ed1d24',
+          hover: '#d6001c',
+        },
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate')],
+} satisfies Config;
+
+export default config;
