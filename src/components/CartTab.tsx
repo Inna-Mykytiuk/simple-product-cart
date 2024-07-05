f80b7ded2d5173ff95e4d4e820523c3979a9bd7a
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-07-05 12:57:32 +0300

    create main component and pages files

---
+++ src/components/CartTab.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CartTab() {
+  return <div>CartTab</div>;
+}
---
+++ src/components/ProductCart.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ProductCart() {
+  return <div>ProductCart</div>;
+}
