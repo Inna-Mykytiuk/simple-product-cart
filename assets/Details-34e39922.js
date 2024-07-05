4cb251d36fa2a9a49f0b5825305bbd475cd1ad38
Inna-Mykytiuk <Inna-Mykytiuk@users.noreply.github.com>
2024-07-05 10:08:20 +0000

    deploy: adb5b29c9a4d5aca9b5123a3ef8decff90eb3703

---
+++ assets/Details-34e39922.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-29ec1341.js";function i(){return t.jsx("div",{children:"Details Page"})}export{i as default};
---
+++ assets/Home-005d2d3a.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-29ec1341.js";function r(){return e.jsx("div",{children:"Home page"})}export{r as default};
