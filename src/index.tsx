4bd23dbfb218cf1cdab383567e3c45b2b38481b8
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-07-08 15:04:47 +0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/simple-product-cart">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
