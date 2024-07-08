8d0756654f774e60268a8b129ee746f085fee913
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-07-08 18:00:02 +0300

    add data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { IoCartOutline } from "react-icons/io5";
 
 export default function Header() {
   return (
-    <header>
-      <div className="container">Header</div>
+    <header className="w-full py-8">
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <Link to="/" className="text-xl font-semibold">
+            Home
+          </Link>
+          <div className="flex justify-center items-center relative w-10 h-10 bg-gray-100 rounded-full">
+            <IoCartOutline className="w-6" />
+            <span className="absolute top-2/3 right-1/2 bg-red-500 text-white text-sm w-5 h-5 rounded-full flex justify-center items-center">
+              0
+            </span>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Home() {
   return (
     <section>
-      <div className="container">Home page</div>
+      <div className="container">
+        <h1 className="text-3xl my-5">List Products</h1>
+      </div>
     </section>
   );
 }
