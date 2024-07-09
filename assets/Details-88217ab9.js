86f9d8907c22ddb492ec4ce6e4bd4a21cf7c9307
Inna-Mykytiuk <Inna-Mykytiuk@users.noreply.github.com>
2024-07-09 07:49:20 +0000

    deploy: 6d05346d299a6827d6f45fa877cb3f88d5e11e18

---
+++ assets/Details-88217ab9.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-941e7313.js";function t(){return e.jsx("section",{children:e.jsx("div",{className:"container",children:"Details page"})})}export{t as default};
--- assets/Details-a62f14a8.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-50452364.js";function t(){return e.jsx("section",{children:e.jsx("div",{className:"container",children:"Details page"})})}export{t as default};
