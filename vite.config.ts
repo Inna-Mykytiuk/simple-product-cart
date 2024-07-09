6d05346d299a6827d6f45fa877cb3f88d5e11e18
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-07-09 10:48:36 +0300

    add base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/simple-product-cart/',
   resolve: {
     alias: { src: '/src' },
   },
