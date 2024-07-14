7d22a3c4f65f4473dd39f18e0a4e08226f14852e
Inna-Mykytiuk <innka-pinnka@ukr.net>
2024-07-14 15:03:06 +0300

    fix bug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
 import ProductCart from "../components/ProductCart";
 import { products } from "../data/products";
+import { ProductProps } from "../types/types";
 
 export default function Home() {
+  const transformedProducts: ProductProps[] = products.map((product) => ({
+    ...product,
+    productId: product.id,
+    quantity: 0, 
+  }));
+
   return (
     <section>
       <div className="container">
         <h1 className="text-3xl text-center mb-6">List Products</h1>
         <div className="grid lg:grid-cols-4 sm:grid-cols-2 gap-5">
-          {products.map((product, key) => (
+          {transformedProducts.map((product, key) => (
             <ProductCart key={key} data={product} />
           ))}
         </div>
